946e3ab3ea02c7de36c9161bca1e41fe1312a1d7
supervincii <74002397+supervincii@users.noreply.github.com>
2025-10-25 22:53:22 +0800

    C++ Primer - Chapter 2 add example on using custom header files

---
+++ examples/ch2/sales_data.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include "Sales_data.h"
+
+int main() {
+    Sales_data data1, data2;
+
+    // code to read into data1 and data2
+    double price = 0; // price per book used to calculate total revenue
+    // read the first transactions: ISBN, number of books sold, price per book
+    std::cin >> data1.bookNo >> data1.units_sold >> price;
+    // calculate the total revenue from price ane units_sold
+    data1.revenue = data1.units_sold * price;
+    // read the second transaction
+    std::cin >> data2.bookNo >> data2.units_sold >> price;
+    data2.revenue = data2.units_sold * price;
+
+    // code to check whether data1 and data2 have the same ISBN
+    if (data1.bookNo == data2.bookNo) {
+        unsigned totalCnt = data1.units_sold + data2.units_sold;
+        double totalRevenue = data1.revenue + data2.revenue;
+        // print ISBN, total sold, total revenue, average price per book
+        std::cout << data1.bookNo << " " << totalCnt << " " << totalRevenue << " ";
+        if (totalCnt != 0) {
+            std::cout << totalRevenue / totalCnt << std::endl;
+        } else {
+            std::cout << "(no sales)" << std::endl;
+            return 0;
+        }
+    } else {
+        std::cerr << "Data must refer to the same ISBN" << std::endl;
+        return -1;
+    }
+    // and if so print the sum of data1 and data2
+}
